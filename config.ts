8a9337969002affd513b4a7e4bccda75b0f49472
Sourish99 <57459663+Sourish99@users.noreply.github.com>
2025-11-25 10:48:45 +0530

    Updated name of owner

--- config.ts
+++ config.ts
@@ -22,9 +22,9 @@ function getDateAndTime(): string {
 export const DATE_AND_TIME = getDateAndTime();
 
 export const AI_NAME = "MyAI3";
-export const OWNER_NAME = "FirstName LastName";
+export const OWNER_NAME = "Sourish Dewani";
 
-export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
+export const WELCOME_MESSAGE = `Yo mama! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
 
 export const CLEAR_CHAT_TEXT = "New";
 
